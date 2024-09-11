359f655683fbdd8355975f83fdfb12e8c3fc076d
William Walker <w_walker@icloud.com>
2024-09-11 16:44:10 -0500

    add support for Spanish

--- hello.go
+++ hello.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	switch language {
+	case "English":
+		return englishHelloPrefix + name
+	case "Spanish":
+		return spanishHelloPrefix + name
+	default:
+		return englishHelloPrefix + name
+	}
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
